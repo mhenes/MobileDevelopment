73ff4566554452572f41086d293996208fead0ac
Henes, Mark <Mark.Henes@ingramcontent.com>
2016-10-25 16:16:18 -0500

    Problem 1: steps 1 and 2 completed.

---
+++ Assignments/Assignment2/Assignment2/Assignment2/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Henes, Mark on 10/25/16.
+//  Copyright © 2016 Henes, Mark. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
