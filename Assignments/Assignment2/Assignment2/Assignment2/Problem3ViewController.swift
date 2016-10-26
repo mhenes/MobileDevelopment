f913b8ccf111fbd9159a77f099490fc4c89ecc62
Henes, Mark <Mark.Henes@ingramcontent.com>
2016-10-25 20:41:59 -0500

    Problem 1: step 6 completed.

---
+++ Assignments/Assignment2/Assignment2/Assignment2/Problem3ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Problem3ViewController.swift
+//  Assignment2
+//
+//  Created by Henes, Mark on 10/25/16.
+//  Copyright © 2016 Henes, Mark. All rights reserved.
+//
+
+import UIKit
+
+class Problem3ViewController: UIViewController {
+    
+}
